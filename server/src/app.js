7230b8399a0d1a28fbfe27456f0cf57070db71e6
Duverly Ivan Mondragón Manchay <ivanmondragonmanchay@gmail.com>
2023-02-26 19:00:44 +0100

    add folder server

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+import express, { json }  from "express";
+import employeesRouters from "./routes/employees.routes.js";
+import indexRouters from "./routes/index.routes.js";
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.use(json());
+app.use('/api',employeesRouters);
+app.use(indexRouters);
+
+// Autor : Duberly Ivan Mondragon Manchay
+
+// Midelware 
+app.use((req,res,next)=>{
+    res.status(404).json({
+        message:'endpoint not found'
+    })
+})
+export default app;
